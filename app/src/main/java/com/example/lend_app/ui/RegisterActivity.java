6a48cea6b51e06c1b2fd08d37739ab02401b53bc
felipe-franca <felipe.paulino57@gmail.com>
2022-11-21 23:26:18 -0300

    reafactor: improved login centralization. seted icons in register actvity

--- app/src/main/java/com/example/lend_app/ui/RegisterActivity.java
+++ app/src/main/java/com/example/lend_app/ui/RegisterActivity.java
@@ -7,12 +7,9 @@ import android.os.Bundle;
 import com.example.lend_app.R;
 
 public class RegisterActivity extends AppCompatActivity {
-  private final String TITLE = "Novo Cadastro";
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_register);
-
-    setTitle(TITLE);
   }
 }
